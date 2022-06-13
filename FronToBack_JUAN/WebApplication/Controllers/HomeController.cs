d4d3bf187fd0a2843b845a0246850d54515108d1
emilhajizade <emilhacizade44@mail.ru>
2022-06-13 11:15:13 +0400

    added little data and bug fix

--- FronToBack_JUAN/WebApplication/Controllers/HomeController.cs
+++ FronToBack_JUAN/WebApplication/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication.DAL;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+         private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            Blogs result = _context.Blogs.FirstOrDefault();
+            return View(result);
         }
     }
 }
